66b0f0ec1dfab8b3e722dff1af86701c2136e122
Kirill <kiril-karp4@mail.ru>
2022-09-12 16:32:49 +1000

    Task 43

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
-
+/*
 Console.Write("Введите числа через запятую: ");
 int[] numbers = StringToNum(Console.ReadLine());
 PrintArray(numbers);
@@ -65,13 +65,13 @@ void PrintArray(int[] array)
     }
     Console.Write("]");
 }
-
+*/
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-/*
+
 Console.WriteLine("Введите значение b1");
 double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число k1");
@@ -86,4 +86,3 @@ double y = k2 * x + b2;
 
 Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 
-*/
